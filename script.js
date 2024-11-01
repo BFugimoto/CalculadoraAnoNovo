044d72b18b938b7b5721538c8d01241c6ed86f23
Bruno Fugimoto <brunofujimoto98@gmail.com>
2024-11-01 10:20:35 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,54 @@
-const dataAtual = document.getElementById("dataAtual");
-const horaAtual = document.getElementById("horaAtual");
-const calcDias = document.getElementById("calcDias");
-const diasLeft = document.getElementById("diasLeft");
+const tarefaInput = document.getElementById("tarefaInput");
+const addTarefa = document.getElementById("addTarefa");
+const msgErro = document.getElementById("msgErro");
 
-const nowDate = new Date();
-const anoNovo = new Date(nowDate.getFullYear() + 1, 0, 1);
+addTarefa.disabled = true;
 
-dataAtual.innerHTML = nowDate.getDate() + "/" + (nowDate.getMonth() + 1) + "/" + nowDate.getFullYear();
+tarefaInput.addEventListener("blur", () => {
+    if(tarefaInput.value == ""){
+        msgErro.innerHTML = "Por favor, insira uma tarefa!";
+        addTarefa.style.cursor = "default";
+        addTarefa.disabled = true;
+    }else if(tarefaInput.value != ""){
+        msgErro.innerHTML = "";
+        addTarefa.style.cursor = "pointer";
+        addTarefa.disabled = false;
+    }
+});
 
-horaAtual.innerHTML = nowDate.getHours() + ":" + nowDate.getMinutes() + ":" + nowDate.getSeconds();
+tarefaInput.addEventListener("focus", () => {
+    msgErro.innerHTML = "";
+});
 
-calcDias.addEventListener("click", () => {
-    const timeDiff = anoNovo - nowDate;
-    const daysDiff = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
-        
-    diasLeft.innerHTML = daysDiff + " dias para o Ano Novo!";
-})
+addTarefa.addEventListener("click", () => {
+
+    const tarefa = document.createElement("p");
+    const btnRemove = document.createElement("button");
+    const tarefaDiv = document.createElement("div");
+    const tarefaLista = document.getElementById("tarefaLista");
+
+    tarefa.innerHTML = tarefaInput.value;
+    tarefa.style.cursor = "pointer";
+    btnRemove.textContent = "Remover";
+    
+    tarefaDiv.classList.add("tarefa");
+    tarefaDiv.appendChild(tarefa);
+    tarefaDiv.appendChild(btnRemove);
+    tarefaLista.appendChild(tarefaDiv);
+    
+    btnRemove.addEventListener("click", () => {
+        tarefaLista.removeChild(tarefaDiv);
+    });
+
+    tarefa.addEventListener("click", () => {
+        tarefa.style.textDecoration = "line-through";
+        tarefa.style.cursor = "default";
+        btnRemove.style.textDecoration = "line-through";
+        btnRemove.disabled = true;
+    });
+
+    tarefaInput.value = ""; // Limpa o input da tarefa
+    msgErro.innerHTML = ""; // Reseta a mensagem de erro
+    addTarefa.disabled = true; // Desbilita o botão de adicionar tarefa
+
+});
